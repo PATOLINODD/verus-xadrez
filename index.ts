3d511e38d272f344a9ba68d5660eecf8b4d0b3d4
Patrick Soares <psodesenhos@gmail.com>
2026-09-22 14:11:32 +0000

    initial setup with bun

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import indexHtml from './frontend/index.html';
+
+const server = Bun.serve({
+    port: 3000,
+    routes: {
+        "/": indexHtml
+    }
+})
+
+console.log(`Server running on: ${server.url}`);
